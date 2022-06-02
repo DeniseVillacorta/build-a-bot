298bde9c55ea9657fea383fa4e53e3fbca061b50
DeniseVillacorta <villacorta9806denise@gmail.com>
2022-06-02 14:26:54 +0800

    created a vuex store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './Router/index';
+import store from './Store/index';
 
 createApp(App)
   .use(router)
+  .use(store)
   .mount('#app');
