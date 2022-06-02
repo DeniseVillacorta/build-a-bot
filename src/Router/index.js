2af805c7938218d60b70472337e34c0c8b24ae63
DeniseVillacorta <villacorta9806denise@gmail.com>
2022-06-02 11:28:41 +0800

    added nested routes

--- src/Router/index.js
+++ src/Router/index.js
@@ -6,6 +6,11 @@ import {
 import HomePage from '../Home/HomePage.vue';
 import RobotBuilder from '../Build/RobotBuilder.vue';
 import PartInfo from '../Parts/PartInfo.vue';
+import BrowseParts from '../Parts/BrowseParts.vue';
+import RobotHeads from '../Parts/RobotHeads.vue';
+import RobotArms from '../Parts/RobotArms.vue';
+import RobotTorsos from '../Parts/RobotTorsos.vue';
+import RobotBases from '../Parts/RobotBases.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -18,6 +23,33 @@ export default createRouter({
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
+  },
+  {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads,
+      },
+      {
+        name: 'BrowseArms',
+        path: 'arms',
+        component: RobotArms,
+      },
+      {
+        name: 'BrowseTorsos',
+        path: 'torsos',
+        component: RobotTorsos,
+      },
+      {
+        name: 'Browsebases',
+        path: 'bases',
+        component: RobotBases,
+      },
+    ],
   },
   {
     path: '/parts/:partType/:id',
