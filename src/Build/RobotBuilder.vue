a7c94f59e982726d6d3dfa6b1da5142c8e03f4a5
DeniseVillacorta <villacorta9806denise@gmail.com>
2022-06-02 13:45:30 +0800

    added navigation guards

--- src/Build/RobotBuilder.vue
+++ src/Build/RobotBuilder.vue
@@ -77,10 +77,21 @@ import CollapsibleSection from '../Shared/CollapisbleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals: 0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -114,6 +125,7 @@ export default {
                    robot.base.cost;
       // eslint-disable-next-line prefer-object-spread
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     },
   },
 };
