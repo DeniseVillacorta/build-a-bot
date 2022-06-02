b66af2466b6f3a217a79109dfbc135e7f0e87755
DeniseVillacorta <villacorta9806denise@gmail.com>
2022-06-02 18:12:11 +0800

    Organizing store by modules

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,36 +1,10 @@
 import { createStore } from 'vuex';
-import axios from 'axios';
+import robotsModule from './Modules/Robots';
+import usersModule from './Modules/Users';
 
 export default createStore({
-  state: {
-    cart: [],
-    parts: null,
-  },
-  mutations: {
-    addRobotToCart(state, robot) {
-      state.cart.push(robot);
-    },
-    updateParts(state, parts) {
-      state.parts = parts;
-    },
-  },
-  actions: {
-    // get parts from API
-    getParts({ commit }) {
-      axios.get('/api/parts')
-        .then((result) => commit('updateParts', result.data))
-        .catch(console.error);
-    },
-    // add robot to cart to API
-    addRobotToCart({ commit, state }, robot) {
-      const cart = [...state.cart, robot];
-      return axios.post('/api/cart', cart)
-        .then(() => commit('addRobotToCart', robot));
-    },
-  },
-  getters: {
-    cartSaleItems(state) {
-      return state.cart.filter((item) => item.head.onSale);
-    },
+  modules: {
+    robots: robotsModule,
+    users: usersModule,
   },
 });
