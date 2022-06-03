b11d8ec01257916756801d5f941f51e28ab2ea30
DeniseVillacorta <villacorta9806denise@gmail.com>
2022-06-03 11:53:44 +0800

    used vuex maphelpers

--- src/Store/index.js
+++ src/Store/index.js
@@ -3,8 +3,13 @@ import robotsModule from './Modules/Robots';
 import usersModule from './Modules/Users';
 
 export default createStore({
+  state: {
+    foo: 'root-foo',
+  },
   modules: {
     robots: robotsModule,
     users: usersModule,
+  },
+  getters: {
   },
 });
