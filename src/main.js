186fde9b7df47fcf712f8c9277c599437e09e1c4
DeniseVillacorta <villacorta9806denise@gmail.com>
2022-06-03 16:24:37 +0800

    declaring filters

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue';
 import router from './Router/index';
 import store from './Store/index';
 import pinDirectives from './Shared/Pin-Directives';
+import currencyFilter from './Shared/Currency-fliter';
 
-createApp(App)
-  .use(router)
+const app = createApp(App);
+
+app.config.globalProperties.$filters = {
+  currency: currencyFilter,
+};
+
+app.use(router)
   .use(store)
   .directive('pin', pinDirectives)
   .mount('#app');
